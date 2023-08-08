254c030cfd3b4f4d334b78e766cdfaf0ea23d10e
andcool-2007 <drolll2007@gmail.com>
2023-08-08 22:51:49 +0300

    u

---
+++ me/script.js
@@ -0,0 +1,54 @@
+function data(uuid_){
+    const url = api + '/load/'+ uuid_
+    
+
+    fetch(url, {method: "GET"})
+    .then(res => res.json())
+    .then(data => {
+        obj = data;
+    })
+    .then(() => {
+        if (obj["message"] == "Success"){
+            document.getElementById("ItemPreview").src = "data:image/png;base64," + String(obj["data"]);
+            if(obj["first_name"] != "None"){
+                var first = obj["first_name"]
+            }else{
+                var first = ""
+            }
+
+            if(first != ""){var kast_a = " "}
+            else{var kast_a = ""}
+
+			if(obj["last_name"] != "None"){
+                var last = kast_a + obj["last_name"]
+            }else{
+                var last = ""
+            }
+			var name = first + last
+            const names = document.querySelector("#name");
+			names.textContent = name;
+        }
+    });
+}
+function logout(){
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+    }
+    const url = api + '/logout/'+ getCookie("user")
+    
+
+    fetch(url, {method: "GET"})
+    .then(res => res.json())
+    .then(data => {
+        obj = data;
+    })
+    .then(() => {
+        if (obj["message"] == "Success"){
+            document.cookie = "user=" + "undefined";
+            location.reload()
+        }
+    });
+
+}
