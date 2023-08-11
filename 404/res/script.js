b3293e883716f2fc77ade429a0159931084691a9
andcool-2007 <drolll2007@gmail.com>
2023-08-11 17:02:41 +0300

    u

--- 404/res/script.js
+++
@@ -1,9 +0,0 @@
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "navig") {
-        x.className += " responsive";
-    } else {
-        x.className = "navig";
-    }
-}
